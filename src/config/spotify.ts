9e82a352fbf6f045b18a1811bdf9fff76d882613
r-lallali <45920249+r-lallali@users.noreply.github.com>
2025-12-09 14:44:50 +0100

    fix spotify.ts

--- src/config/spotify.ts
+++ src/config/spotify.ts
@@ -1,6 +1,6 @@
 export const spotifyConfig = {
   clientId: import.meta.env.VITE_SPOTIFY_CLIENT_ID || "",
-  redirectUri: import.meta.env.VITE_SPOTIFY_REDIRECT_URI || "",
+  redirectUri: import.meta.env.VITE_REDIRECT_URI || "",
 };
 
 if (!spotifyConfig.clientId || !spotifyConfig.redirectUri) {
